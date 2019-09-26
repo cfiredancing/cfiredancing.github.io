aa0e7c3fbde0e4d0bd92c61724479d7ec2c9e098
cfiredancing <55114906+cfiredancing@users.noreply.github.com>
2019-09-26 00:36:57 -0400

    Create 2019-09-25-post.c

---
+++ _posts/2019-09-25-post.c
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Sweater Weather
+tags: [computer science]
+comments: true
+---
+
+# A tip for past Calen: Triple-check syntax
+
+
+![Sewing Sampler](https://cfiredancing.github.io/img/IMG_3711.JPG)
+
